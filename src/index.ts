34222601451c485cfb9c09b853fe694694974f0f
Baris Tuna <baristna@gmail.com>
2020-09-13 02:13:49 +0300

    version update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
-import { useEffect, useState } from 'react';
-import { IuseCustomHook } from './types';
+import { useState, useEffect } from 'react';
 
-const useCustomHook:IuseCustomHook = (initialState) => {
-  const [state, setState] = useState(initialState);
+function useClamp<T extends number | number[]>(value:T, min?:number, max?:number):T {
+  const [clampedValue, setClampedValue] = useState(value);
+
+  const singleValueClamp = () => {
+    if ((min !== undefined) && (value < min)) {
+      setClampedValue(min as T);
+    } else if (max !== undefined && (value > max)) {
+      setClampedValue(max as T);
+    } else {
+      setClampedValue(value);
+    }
+  };
+
+  const multipleValueClamp = () => {
+    if (Array.isArray(value)) {
+      let balanced = value.map(val => (min !== undefined && (val < min)) ? min : val);
+      const total = balanced.reduce((a, c) => a + c);
+      if ((max !== undefined) && (total > max)) {
+        balanced = balanced.map(val => (max * (val / total)));
+      }
+      setClampedValue(balanced as T);
+    }
+  };
 
   useEffect(() => {
-    setState(initialState)
-  }, [initialState])
+    if (typeof value === 'number') {
+      singleValueClamp();
+    } else {
+      multipleValueClamp();
+    }
+  }, [value, min, max]);
 
-  return [state, setState]
-};
+  return clampedValue;
+}
 
-export default useCustomHook
+export default useClamp;
